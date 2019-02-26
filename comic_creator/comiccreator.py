1490197ca01bd8e4e98da06c44eaaac062235d40
Matias Borghi <borghi.matias@gmail.com>
2019-02-26 12:04:32 -0300

    new project comic creator

---
+++ comic_creator/comiccreator.py
@@ -0,0 +1,20 @@
+ # File name: comiccreator.py
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
+
+# There is no need to explicitely load comiccreator.kv because Kivy automatically loads it by extracting the first part of the ComicCreatorApp name.
+Builder.load_file('toolbox.kv')
+Builder.load_file('drawingspace.kv')
+Builder.load_file('generaloptions.kv')
+Builder.load_file('statusbar.kv')
+
+class ComicCreator(AnchorLayout):
+	pass
+
+class ComicCreatorApp(App):
+	def build(self):
+		return ComicCreator()
+
+if __name__ == "__main__":
+	ComicCreatorApp().run()
